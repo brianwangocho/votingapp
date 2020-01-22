7199f517654cc2fa56d136149ea6f8ab538a0413
wangocho <bwnagocho@gmail.com>
2020-01-22 19:27:06 +0300

    ui update on card view and add candidate

--- app/src/main/java/com/example/voting/Dashboard.java
+++ app/src/main/java/com/example/voting/Dashboard.java
@@ -9,7 +9,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
